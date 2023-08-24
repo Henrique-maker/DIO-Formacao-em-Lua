1fa3bc7c387ced1e468e5ec4e9cb3ac628bf9ac5
Henrique-maker <henrique.fidelis.dnv@gmail.com>
2023-08-24 17:50:53 -0300

    iniciando segundo modulo

---
+++ 2-Avançando-na-Linguagem-Lua/main.lua
@@ -0,0 +1,11 @@
+--[[
+    Funções sem parametros e sem retorno.
+    Funções sem parametros e com retorno.
+    Funções com parametros e sem retorno.
+    Funções com parametros e com retorno.
+]]
+
+
+local function sayHello()
+    print("Olá usuário, seja bem-vindo!")
+end
